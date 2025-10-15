bed2386272bcc919974cc83de8f1e65117b81ae3
NSMarcely <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>
2025-10-14 21:07:00 -0300

    Agora os atributos são privados da classe Pessoa

--- pessoa.py
+++ pessoa.py
@@ -1,1 +1,8 @@
-print(220)
+class Pessoa:
+    def __init__(self, nome: str, matricula: str, senha: str):
+        self.nome = nome
+        self.matricula = matricula
+        self.senha = senha
+
+    def __str__(self):
+        return f"{self.nome} ({self.matricula})"
