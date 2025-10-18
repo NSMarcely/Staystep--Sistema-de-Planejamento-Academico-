5274c3ec90907b23427f205177830e1bed8e4e56
NSMarcely <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>
2025-10-18 09:40:35 -0300

    Foi adicionado os metodos de acesso na classe Pessoa

--- pessoa.py
+++ pessoa.py
@@ -1,8 +1,14 @@
 class Pessoa:
-    def __init__(self, nome: str, matricula: str, senha: str):
+    def __init__(self, nome: str, senha: str):
         self.__nome = nome
-        self.__matricula = matricula
         self.__senha = senha
-
+    def get_nome(self):
+        return self.__nome
+    def set_nome(self, nome):
+        self.__nome = nome
+    def get_senha(self):
+        return self.__senha 
+    def set_senha(self, senha):
+        self.__senha = senha        
     def __str__(self):
-        return f"{self.__nome} ({self.__matricula})"
+        return f"{self.__nome}"
